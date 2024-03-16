ecc286081396ab946c40b816e3d7256d53cd306f
Md Shahid Ali <101797831+deepmachine786@users.noreply.github.com>
2024-03-16 12:38:20 +0000

    update site but under maintance due to private

---
+++ campusconnect/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Welcome to Campus Connect. This site under maintance.
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
